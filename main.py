18bf70a4dbb0c4ff6ecb7f97ce8461b1d56dd525
Shahrukh Anwar <shahrukh.anwar@senecaglobal.com>
2023-02-04 14:41:44 +0530

    New code added for OOPs design

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class IProduct:
+    def get_price(self):
+        pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Product(IProduct):
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def get_price(self):
+        return self._price
+
+
+
